15fd49b094c8a4040a4909b129e4b29a727fe02a
ericpo1sh <ericpo1sh@gmail.com>
2023-08-21 12:43:12 -0700

    test

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ *
+ *
+ *
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (!tree)
+		return (0);
+
+	if (!tree->parent)	
+		depth = binary_tree_depth(depth) + 1;
+
+	return (depth);
+}

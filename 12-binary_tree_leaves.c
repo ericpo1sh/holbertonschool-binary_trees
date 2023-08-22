0a3d32006c9e659d5afaee838ac27e5bf3279e3a
ericpo1sh <ericpo1sh@gmail.com>
2023-08-22 09:34:30 -0700

    Write a function that counts the leaves in a binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -14,13 +14,14 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
+	if (!tree->left && !tree->right)
+		return (1);
+
 	if (tree->right)
 		right = binary_tree_leaves(tree->right);
 
 	if (tree->left)
 		left = binary_tree_leaves(tree->left);
 
-	if (!tree->left || !tree->right)
-		return (left + right);
-	return (1);
+	return (left + right);
 }

bb00dcfaf771b53fcfb705b4672511a424cfc53e
ericpo1sh <ericpo1sh@gmail.com>
2023-08-22 12:07:30 -0700

    Write a function that checks if a binary tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -14,12 +14,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	left = binary_tree_is_full(tree->left);
-	right = binary_tree_is_full(tree->right);
-
 	if ((!left || !right) || (left && right))
 		return (1);
 
+	left = binary_tree_is_full(tree->left);
+	right = binary_tree_is_full(tree->right);
+
 	return (left * right);
 }
-

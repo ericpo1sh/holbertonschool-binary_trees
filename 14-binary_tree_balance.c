bcf089457021e28430cae52f432162409b6f51ac
ericpo1sh <ericpo1sh@gmail.com>
2023-08-22 11:21:49 -0700

    test

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- *
- *
- *
+ * binary_tree_balance - Checks balance between left and right sides of root
+ * @tree- root pointer
+ * Return: 0 if fail, Balance if success
  */
 
 int binary_tree_balance(const binary_tree_t *tree)
